9593e63e17f21ef34e0bff3dc85af6c9f59bb859
MiguelRJ <miguel.rj96@gmail.com>
2017-10-30 13:08:10 +0100

    boton save de action bar

--- app/src/main/java/com/example/inventory/SectorActivity.java
+++ app/src/main/java/com/example/inventory/SectorActivity.java
@@ -1,11 +1,12 @@
 package com.example.inventory;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 import com.example.inventory.adapter.SectorAdapter;
 
 /**
@@ -31,5 +32,12 @@ public class SectorActivity extends AppCompatActivity {
         recyclerSector.setLayoutManager( new GridLayoutManager(this,2));
         adapter = new SectorAdapter();
         recyclerSector.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_activity_sector,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
