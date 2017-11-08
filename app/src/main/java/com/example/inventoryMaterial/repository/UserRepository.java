5f349ba1d231c4186851db48d87130da27845237
MiguelRJ <miguel.rj96@gmail.com>
2017-11-08 14:42:50 +0100

    User, UserRepository, jUnit

---
+++ app/src/main/java/com/example/inventoryMaterial/repository/UserRepository.java
@@ -0,0 +1,41 @@
+package com.example.inventoryMaterial.repository;
+
+import com.example.inventoryMaterial.pojo.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by usuario on 8/11/17.
+ */
+
+public class UserRepository {
+    private static UserRepository userRepository;
+    private ArrayList<User> users;
+
+    private UserRepository() {
+        users = new ArrayList<>();
+        initialice();
+    }
+
+    private void initialice() {
+        saveUser(new User(1,"Migue1","123","Miguel Rodriguez Jimenez", "miguel1@gmail.com",false,false));
+        saveUser(new User(2,"Migue2","123","Miguel Rodriguez Jimenez", "miguel2@gmail.com",true,false));
+        saveUser(new User(3,"Migue3","123","Miguel Rodriguez Jimenez", "miguel3@gmail.com",false,true));
+        saveUser(new User(4,"Migue4","123","Miguel Rodriguez Jimenez", "miguel4@gmail.com",true,true));
+    }
+
+    private void saveUser(User user){
+        users.add(user);
+    }
+
+    public static UserRepository getInstance(){
+        if(userRepository == null) {
+            userRepository = new UserRepository();
+        }
+        return userRepository;
+    }
+
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+}
--- app/src/test/java/com/example/inventoryMaterial/ExampleUnitTest.java
+++ app/src/test/java/com/example/inventoryMaterial/ExampleUnitTest.java
@@ -10,8 +10,36 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    //1. El usuario introduce usuario/email
+
+    //2. El usuario introduce una contraseña
+
+    //3. El usuario exista en la base de datos (Repository)
+
+    //4. La contraseña sea correcta
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void signIn_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+    }
+
+    //1. El usuario introduce usuario
+
+    //2. El usuario introduce una contraseña
+
+    //5. El usuario introduce un email
+
+    //6. La contraseña tiene al menos 6 caracteres
+
+    //7. El usuario no esxiste en la base de datos (Repository)
+
+    //8. El email no existe en la base de datos (Repository)
+
+    //9. Doble comprobacion de la contraseña
+    @Test
+    public void signUn_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+
     }
 }
