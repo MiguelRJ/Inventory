f3e9f663c32d9c2bd4c1bad3d33678d0d49d776f
MiguelRJ <miguel.rj96@gmail.com>
2017-11-10 13:06:17 +0100

    Pruebas del login

--- app/src/androidTest/java/com/example/inventoryMaterial/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/inventoryMaterial/ExampleInstrumentedTest.java
@@ -1,12 +1,25 @@
 package com.example.inventoryMaterial;
 
 import android.content.Context;
+import android.support.annotation.StringRes;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import android.support.test.espresso.Espresso;
 
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static org.junit.Assert.*;
 
 /**
@@ -27,7 +40,8 @@ public class ExampleInstrumentedTest {
      */
     @Test
     public void isUserEmpty() {
-
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorUserEmpty);
     }
 
     /**
@@ -35,7 +49,8 @@ public class ExampleInstrumentedTest {
      */
     @Test
     public void isEmailEmpty() {
-
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorEmailEmpty);
     }
 
     /**
@@ -43,7 +58,8 @@ public class ExampleInstrumentedTest {
      */
     @Test
     public void isPasswordEmpty() {
-
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorPasswordEmpty);
     }
 
     /**
@@ -51,7 +67,10 @@ public class ExampleInstrumentedTest {
      */
     @Test
     public void passwordLenght() {
-
+        onView(withId(R.id.edtUser)).perform(typeText("lourdes"),closeSoftKeyboard());
+        onView(withId(R.id.edtPassword)).perform(typeText("lu"),closeSoftKeyboard());
+        onView(withId(R.id.btnSignIn)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorPasswordLength);
     }
 
     /**
@@ -61,4 +80,9 @@ public class ExampleInstrumentedTest {
     public void passwordDoubleCheck() {
 
     }
+
+    private void checkSnackBarDisplayByMessage(@StringRes int message) {
+        onView(withText(message)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
 }
